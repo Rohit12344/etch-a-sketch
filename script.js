e93d103dfea235bc7a3637332a19016242592425
Rohit <rohit.rgurung@gmail.com>
2022-12-04 21:18:09 +0545

    Add all features

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let gridSize;
 const container = document.querySelector('.container');
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
 function createGrid(grid_size)
 {
@@ -36,10 +36,7 @@ function createGrid(grid_size)
             {
                 boxes.style.borderBottomWidth = '3px';
             }
-    
-            boxes.addEventListener('mouseenter', () => {
-                boxes.style.backgroundColor = 'black';
-            })
+            boxes.style.backgroundColor = 'rgb(255,255,255)'
             row.appendChild(boxes);
         }
         container.appendChild(row);
@@ -48,12 +45,102 @@ function createGrid(grid_size)
 
 createGrid(16);
 
-button.addEventListener('click', () => {
+function listenerForDraw(e)
+{
+    e.target.style.backgroundColor = 'rgb(0,0,0)';
+}
+
+function draw()
+{
+    document.querySelectorAll('.boxes').forEach((box) => {
+        box.removeEventListener('mouseenter',listenerForShadeDraw);
+        box.removeEventListener('mouseenter', listenerForEraser);
+        box.removeEventListener('mouseenter',listenerForColorfulDraw);
+        box.addEventListener('mouseenter', listenerForDraw);
+    });
+}
+
+function listenerForEraser(e)
+{
+    e.target.style.backgroundColor = 'rgb(255,255,255)';
+}
+
+function eraser()
+{
+    const boxes = document.querySelectorAll('.boxes');
+
+    boxes.forEach((box) => {
+        box.removeEventListener('mouseenter',listenerForDraw);
+        box.removeEventListener('mouseenter',listenerForShadeDraw);
+        box.removeEventListener('mouseenter',listenerForColorfulDraw);
+        box.addEventListener('mouseenter', listenerForEraser);
+    });
+}
+
+function listenerForColorfulDraw(e)
+{
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+}
+
+function colorfulDraw()
+{
+    const boxes = document.querySelectorAll('.boxes');
+    boxes.forEach((box) => {
+        box.removeEventListener('mouseenter',listenerForDraw);
+        box.removeEventListener('mouseenter',listenerForEraser);
+        box.removeEventListener('mouseenter',listenerForShadeDraw);
+        box.addEventListener('mouseenter', listenerForColorfulDraw);
+    });   
+}
+
+function listenerForShadeDraw(e)
+{
+    let rgbValue = e.target.style.getPropertyValue('background-color');
+    let rgbValueArray = rgbValue.slice(4,rgbValue.length-1).split(',');
+
+    e.target.style.backgroundColor = `rgb(${Math.floor(rgbValueArray[0] - 0.1 * rgbValueArray[0])},${Math.floor(rgbValueArray[1] - 0.1 * rgbValueArray[1])},${Math.floor(rgbValueArray[2] - 0.1 * rgbValueArray[2])})`;
+}
+
+function shadeDraw()
+{
+    document.querySelectorAll('.boxes').forEach((box) => {
+        box.removeEventListener('mouseenter',listenerForDraw);
+        box.removeEventListener('mouseenter',listenerForEraser);
+        box.removeEventListener('mouseenter',listenerForColorfulDraw);
+        box.addEventListener('mouseenter', listenerForShadeDraw);
+    });
+}
+
+buttons[0].addEventListener('click', () => {
     gridSize = +(prompt('Enter how many squares do you want in a row & a column: '));
-    createGrid(gridSize);
+    if(gridSize)
+    {
+        createGrid(gridSize);
+    }
+    else
+    {
+        createGrid(16);
+    }
 })
 
+buttons[1].addEventListener('click' , () => {
+    if(gridSize)
+    {
+        createGrid(gridSize);
+    }
+    else
+    {
+        createGrid(16);
+    }
+})
 
+buttons[2].addEventListener('click', eraser);
 
+buttons[3].addEventListener('click', draw);
 
+buttons [4].addEventListener('click' , colorfulDraw);
 
+buttons[5].addEventListener('click', shadeDraw);
